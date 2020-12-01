b5de22c2063de4dd052199a5686f92cb51113a24
Luís Fernando <luis1407@live.com>
2020-12-01 10:04:13 -0300

    Added struct of project

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome Luís Fernnado!</Text>
       <StatusBar style="auto" />
     </View>
   );
